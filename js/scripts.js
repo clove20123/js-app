644cc159244a7f19646574429f8b8af484af9759
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-03-11 14:02:24 +0100

    added pokemon repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+let pokemonRepository = (function () {
 let pokemonList = [
   { name: Charmander, height: 0.6, types: ['fire']},
   { name: Charmeleon, height: 1.1, types: ['fire']},
   { name: Charizard, height: 1.7, types: ['fire', 'flying']},
 ];
+
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
+)}
