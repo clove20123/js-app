7ec17c50c284275776e13f491d7e770f10cf3d70
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-03-16 16:27:44 +0100

    Made pokemon names strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 let repository = [
-  { name: Charmander, height: 0.6, types: ['fire']},
-  { name: Charmeleon, height: 1.1, types: ['fire']},
-  { name: Charizard, height: 1.7, types: ['fire', 'flying']},
+  { name: 'Charmander', height: 0.6, types: ['fire']},
+  { name: 'Charmeleon', height: 1.1, types: ['fire']},
+  { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
   ];
 
   function printArrayDetails() {
     for (let i = 0; i < repository.length; i++) {
-      document.write("<p>" + repository[i].name + "</p>")
+      if (repository.size > 1.2) {
+        document.write('This is a big pokemon')
+      }
+      document.write("<p>" + repository[i].name, repository[i].height, repository[i].types + "</p>")
     }
   }
 
-  printArrayDetails();
-  printArrayDetails();
+printArrayDetails();
