a7673bd57b1dfdf25cca72bb5248bb7623d6cbbf
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-03-18 16:07:17 +0100

    added repository and replaced the for loop with a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
-let repository = [
+//defining pokemon list
+let pokemonList = [
   { name: 'Charmander', height: 0.6, types: ['fire']},
   { name: 'Charmeleon', height: 1.1, types: ['fire']},
   { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
   ];
 
-function printArrayDetails() {
-  for (let i = 0; i < repository.length; i++) {
-    if (repository[i].height > 1.2) {
-      document.write("<p>" + repository[i].name, repository[i].height, repository[i].types + 'This is a big pokemon' + "</p>")
-    }
-    else {
-      document.write("<p>" + repository[i].name, repository[i].height, repository[i].types + "</p>")
-    }
-  }
-}
+  let pokemonRepository = (function () {
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
 
-printArrayDetails();
+//for each loop to display pokemonList in browser
+(function () {
+  pokemonList.forEach(function(pokemon) {
+    let pokemonName = pokemon.name
+    let pokemonHeight = pokemon.height
+    let pokemonTypes = pokemon.types
+    // adding wow thats a big pokemon if height is greater than 1.2
+    if(pokemonHeight > 1.2) {
+      document.write(pokemonName + pokemonHeight + pokemonTypes + 'Wow that is a big pokemon');
+      else {
+        document.write(pokemonName + pokemonHeight + pokemonTypes);
+      }
+    }
+  });
+})();
