a8d6e2880e0a6af85daf51b50b99032b2ca41908
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-03-21 18:17:06 +0100

    Added pokemonList inside the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-//defining pokemon list
-let pokemonList = [
-  { name: 'Charmander', height: 0.6, types: ['fire']},
-  { name: 'Charmeleon', height: 1.1, types: ['fire']},
-  { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-  ];
-
-  let pokemonRepository = (function () {
+//IIFE
+let pokemonRepository = (function () {
+  //defining pokemon list
+  let pokemonList = [
+    { name: 'Charmander', height: 0.6, types: ['fire']},
+    { name: 'Charmeleon', height: 1.1, types: ['fire']},
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+    ];
     return {
       add: function(pokemon) {
         pokemonList.push(pokemon);
@@ -17,17 +17,15 @@ let pokemonList = [
   })();
 
 //for each loop to display pokemonList in browser
-(function () {
-  pokemonList.forEach(function(pokemon) {
-    let pokemonName = pokemon.name
-    let pokemonHeight = pokemon.height
-    let pokemonTypes = pokemon.types
-    // adding wow thats a big pokemon if height is greater than 1.2
-    if(pokemonHeight > 1.2) {
-      document.write(pokemonName + pokemonHeight + pokemonTypes + 'Wow that is a big pokemon');
-      else {
-        document.write(pokemonName + pokemonHeight + pokemonTypes);
-      }
+pokemonRepository.getAll().forEach(function(pokemon) {
+  let pokemonName = pokemon.name
+  let pokemonHeight = pokemon.height
+  let pokemonTypes = pokemon.types
+// adding wow thats a big pokemon if height is greater than 1.2
+  if(pokemonHeight > 1.2) {
+    document.write(pokemonName + pokemonHeight + pokemonTypes + 'Wow that is a big pokemon <br>');
     }
-  });
-})();
+    else {
+      document.write(pokemonName + pokemonHeight + pokemonTypes + '<br>');
+    }
+});
